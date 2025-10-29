3cb37678d38e7343c76da09acbabcc8199a4238e
Achraf Jarhou <achraf.jarhou@laplateforme.io>
2025-10-29 10:54:20 +0100

    ajout dune nouvele version qui affiche les jour de la semaine avec un forEach

--- jour01/job05/script.js
+++ jour01/job05/script.js
@@ -1,3 +1,10 @@
+// Créez une fonction “afficherjourssemaines”. Cette fonction ne prend pas de paramètre.
+// Créez un tableau de strings “jourssemaines” qui contient l’ensemble des jours de la
+// semaine, du Lundi au Dimanche. Ensuite à l’aide d’une boucle for (for!)Affichez un par
+// un ces jours.
+
+//! version avec une boucle forEach
+
 function afficherjourssemaines() {
   const jourssemaines = [
     "Lundi",
@@ -8,8 +15,27 @@ function afficherjourssemaines() {
     "Samedi",
     "Dimanche",
   ];
-  for (let i = 0; i < jourssemaines.length; i++) {
-    console.log(jourssemaines[i]);
-  }
+  jourssemaines.forEach((jour) => {
+    console.log(jour);
+  });
 }
+
 afficherjourssemaines();
+
+//! version avec une boucle for
+
+// function afficherjourssemaines() {
+//   const jourssemaines = [
+//     "Lundi",
+//     "Mardi",
+//     "Mercredi",
+//     "Jeudi",
+//     "Vendredi",
+//     "Samedi",
+//     "Dimanche",
+//   ];
+//   for (let i = 0; i < jourssemaines.length; i++) {
+//     console.log(jourssemaines[i]);
+//   }
+// }
+// afficherjourssemaines();
