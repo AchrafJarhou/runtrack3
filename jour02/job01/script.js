0996a305e51052342bc3c34f2353aa7078d689aa
Achraf Jarhou <achraf.jarhou@laplateforme.io>
2025-10-29 13:45:00 +0100

    ajout des erreur une fois que les elemnt sont pas recuperer

--- jour02/job01/script.js
+++ jour02/job01/script.js
@@ -1,8 +1,20 @@
+// Indique que le script est chargé (utile pour le debug dans la console du navigateur)
+console.log("script chargé");
+
 const button = document.querySelector("#btn");
 
-button.addEventListener("click", citation);
+if (button) {
+  // passer la fonction directement comme callback
+  button.addEventListener("click", citation);
+} else {
+  console.error("Element #btn introuvable");
+}
 
 function citation() {
   const article = document.querySelector("#citation");
+  if (!article) {
+    console.error("Element #citation introuvable");
+    return;
+  }
   console.log(article.textContent);
 }
