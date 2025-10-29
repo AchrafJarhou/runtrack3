01500939956ad8bba013f984aec991d685de35b8
Achraf Jarhou <achraf.jarhou@laplateforme.io>
2025-10-29 10:36:18 +0100

    cosnole log dune anné si elle est bisextile ou pas

---
+++ jour01/job04/script.js
@@ -0,0 +1,15 @@
+// Déclarez une fonction “bisextile” qui prend en paramètre une variable “année”. Si l’année
+// est bisextile, la fonction retourne true, sinon elle retourne false.
+// une année bisextile est une année qui a 366 jours au lieu de 365. Une année est bisextile si elle est
+// divisible par 4 mais pas par 100, ou si elle est divisible par 400.
+function bisextile(annee) {
+  if ((annee % 4 === 0 && annee % 100 !== 0) || annee % 400 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+// Test de la fonction
+console.log(bisextile(2020)); // true
+console.log(bisextile(1900)); // false
+console.log(bisextile(2000)); // true
