c370625a483fb90fcc067625d33cd3ad0f832f42
Achraf Jarhou <achraf.jarhou@laplateforme.io>
2025-10-29 13:58:26 +0100

    function qui ajoute un article et laffiche et le masque en cliquant sur un bouton

--- jour02/job01/script.js
+++ jour02/job01/script.js
@@ -1,11 +1,9 @@
-// Indique que le script est chargé (utile pour le debug dans la console du navigateur)
-console.log("script chargé");
 
+console.log("script chargé");
 const button = document.querySelector("#btn");
 
 if (button) {
-  // passer la fonction directement comme callback
-  button.addEventListener("click", citation);
+    button.addEventListener("click", citation);
 } else {
   console.error("Element #btn introuvable");
 }
---
+++ jour02/job02/script.js
@@ -0,0 +1,24 @@
+console.log("script chargé");
+const button = document.querySelector("#btn");
+if (button) {
+  button.addEventListener("click", showhide);
+} else {
+  console.error("Element #btn introuvable");
+}
+
+function showhide() {
+  const button = document.querySelector("#btn");
+  const article = document.querySelector("#citation");
+  if (!article) {
+    console.error("Element #citation introuvable");
+    return;
+  }
+  if (article.style.display === "none") {
+    article.style.display = "block";
+    article.textContent =
+      "L'important n'est pas la chute, mais l'atterrissage.";
+    button.textContent = "cacher l'article";
+  } else {
+    article.style.display = "none";
+  }
+}
