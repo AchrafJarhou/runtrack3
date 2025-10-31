cf8305a84d6678ea0d13aa9d2b417c9f556498b4
Achraf Jarhou <achraf.jarhou@laplateforme.io>
2025-10-31 12:01:35 +0100

    ajout description de lexercice

--- jour04/job02/script.js
+++ jour04/job02/script.js
@@ -1,3 +1,7 @@
+// Créez une fonction javascript “jsonValueKey()” qui prend en paramètre une chaîne de
+// caractères au format json et une clé.
+// Cette fonction retourne la valeur liée à cette clé dans la chaîne de caractères.
+
 const data = `{
   "name": "La Plateforme_",
   "address": "8 rue d'hozier",
